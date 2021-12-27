1e8f1ef2a8c371b79cb200c4f2f8e921b66b6e22
yechu <yechukim217@gmail.com>
2021-12-27 23:00:06 +0900

    create dino obj

--- app.js
+++ app.js
@@ -1,32 +1,77 @@
+// Create Dino Constructor
+function Dino(dino) {
+  this.species = dino.species,
+    this.height = dino.height,
+    this.weight = dino.weight,
+    this.diet = dino.diet,
+    this.where = dino.where,
+    this.when = dino.when,
+    this.fact = dino.fact,
+    this.imgSrc = `/images/${dino.species.toLowerCase()}.png`
+}
 
-    // Create Dino Constructor
+// Create Dino Objects
+let dinosArray = []
+let singleDino
+fetch('dino.json')
+  .then(res => res.json())
+  .then(dinos =>
+    dinos['Dinos'].map(dino =>
+      dinosArray.push(new Dino(dino))
+
+    ))
+
+// Create Human Object
+let human = {}
+
+// Use IIFE to get human data from form
+
+function getValue(id) {
+  return document.getElementById(id).value
+
+}
+const human = (function () {
+  const name = getValue('name')
+  const feet = getValue('feet')
+  const inches = getValue('inches')
+  const weight = getValue('weight')
+  const diet = getValue('diet')
+
+}())
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
+function compareDiet(human, dino) {
+  if (human.diet === dino.diet)
+    return dino.species
+}
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+function compareWeight(human, dino) {
+
+}
 
 
-    // Create Dino Objects
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+function getPigeonFact() {
 
+}
 
-    // Create Human Object
+// Generate Tiles for each Dino in Array
+const tile = `<div></div>`
+// Add tiles to DOM
 
-    // Use IIFE to get human data from form
+const grid = document.getElementById('grid')
 
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
+// Remove form from screen
+const form = document.getElementById('form-container')
 
 
 // On button click, prepare and display infographic
+const btn = document.getElementById('btn')
+btn.addEventListener('click', () => {
+  console.log(dinosArray)
+
+})
